0929a30424d8ce4c101a7a8c1b8ffa9b06828d7e
Dipl. Ing. Péter Varkoly <peter@varkoly.de>
2019-09-19 10:40:32 +0200

    Reboot after first boot.

--- src/clients/inst_oss-setup.rb
+++ src/clients/inst_oss-setup.rb
@@ -6,8 +6,9 @@ module Yast
   class InstOssSetup < Client
     def main
         SCR.Execute(path(".target.bash"), "touch /var/lib/YaST2/reconfig_system")
-        SCR.Write(path(".sysconfig.firstboot.FIRSTBOOT_CONTROL_FILE"), "/etc/YaST2/oss-firstboot.xml")
-        SCR.Write(path(".sysconfig.firstboot.LICENSE_REFUSAL_ACTION"), "continue")
+        SCR.Write(path(".sysconfig.firstboot.FIRSTBOOT_CONTROL_FILE"),  "/etc/YaST2/oss-firstboot.xml")
+        SCR.Write(path(".sysconfig.firstboot.LICENSE_REFUSAL_ACTION"),  "continue")
+        SCR.Write(path(".sysconfig.firstboot.FIRSTBOOT_FINISH_ACTION"), "reboot")
         SCR.Write(path(".sysconfig.firstboot"), nil)
         :next
     end
