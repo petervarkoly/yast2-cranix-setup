81920a93ae6eb1fec4a941a120e3a17178974e50
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-06-11 12:43:58 +0200

    Reorganize inst_cranix to cranix_role_finish

--- src/lib/y2system_role_handlers/cranix_role_finish.rb
+++ src/lib/y2system_role_handlers/cranix_role_finish.rb
@@ -1,19 +1,17 @@
 # encoding: utf-8
 # ------------------------------------------------------------------------------
-# Copyright (c) 2017 Dipl. Ing. Peter Varkoly, Nuernberg, Germany.
-
-module Yast
-  class InstCranixSetup < Client
-    def main
+# Copyright (c) 2020 Dipl. Ing. Peter Varkoly, Nuernberg, Germany.
+require "yast"
+module Y2SystemRoleHandlers
+  class CranixRoleFinish < Client
+    def run
         SCR.Execute(path(".target.bash"), "touch /var/lib/YaST2/reconfig_system")
         SCR.Write(path(".sysconfig.firstboot.FIRSTBOOT_CONTROL_FILE"),  "/etc/YaST2/cranix-firstboot.xml")
         SCR.Write(path(".sysconfig.firstboot.LICENSE_REFUSAL_ACTION"),  "continue")
         SCR.Write(path(".sysconfig.firstboot.FIRSTBOOT_FINISH_ACTION"), "reboot")
         SCR.Write(path(".sysconfig.firstboot"), nil)
-        :next
+        true
     end
   end
 end
-
-Yast::InstCranixSetup.new.main
 
