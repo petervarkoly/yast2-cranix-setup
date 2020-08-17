3873b082397a7bccb64b7bc22cf88c31017c66a0
Dipl. Ing. Péter Varkoly <pvarkoly@cephalix.eu>
2020-08-17 23:00:57 +0200

    Add missed file

---
+++ src/clients/inst_cranix-setup.rb
@@ -0,0 +1,19 @@
+# encoding: utf-8
+# ------------------------------------------------------------------------------
+# Copyright (c) 2017 Dipl. Ing. Peter Varkoly, Nuernberg, Germany.
+Yast.import 'Popup'
+module Yast
+  class InstCranixSetup < Client
+    def main
+        SCR.Execute(path(".target.bash"), "touch /var/lib/YaST2/reconfig_system")
+        SCR.Write(path(".sysconfig.firstboot.FIRSTBOOT_CONTROL_FILE"),  "/etc/YaST2/cranix-firstboot.xml")
+        SCR.Write(path(".sysconfig.firstboot.LICENSE_REFUSAL_ACTION"),  "continue")
+        SCR.Write(path(".sysconfig.firstboot.FIRSTBOOT_FINISH_ACTION"), "reboot")
+        SCR.Write(path(".sysconfig.firstboot"), nil)
+        :next
+    end
+  end
+end
+
+Yast::InstCranixSetup.new.main
+
